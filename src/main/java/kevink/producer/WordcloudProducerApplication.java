1c6c8f7b8ed4c41a56e048bebca8c1e8d3ab88a3
Kevin Kliimask <kevin.kliimask@ut.ee>
2023-03-22 12:35:02 +0200

    Initialize project

---
+++ src/main/java/kevink/producer/WordcloudProducerApplication.java
@@ -0,0 +1,13 @@
+package kevink.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordcloudProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordcloudProducerApplication.class, args);
+	}
+
+}
